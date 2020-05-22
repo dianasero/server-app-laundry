1beeb2368031275fde10d7256cfa68deaacbc9a6
dianasero <dianalehm.15@gmail.com>
2020-05-22 02:27:22 -0500

    distancias-lugares

--- server.js
+++ server.js
@@ -77,6 +77,38 @@ function leer(path){
   }
   return i;
 }
+
+var distance = require('google-distance-matrix');
+distance.key('AIzaSyBREvDnnimfoMMfXQHYuerVt6HucSQZFW4');
+
+var origins = ['San Francisco CA', '40.7421,-73.9914'];
+var destinations = ['New York NY', 'Montreal', '41.8337329,-87.7321554', 'Honolulu'];
+
+distance.units('metric');
+
+distance.matrix(origins, destinations, function (err, distances) {
+    if (err) {
+        return console.log(err);
+    }
+    if(!distances) {
+        return console.log('no distances');
+    }
+    if (distances.status == 'OK') {
+        for (var i=0; i < origins.length; i++) {
+            for (var j = 0; j < destinations.length; j++) {
+                var origin = distances.origin_addresses[i];
+                var destination = distances.destination_addresses[j];
+                if (distances.rows[0].elements[j].status == 'OK') {
+                    var distance = distances.rows[i].elements[j].distance.text;
+                    console.log('Distance from ' + origin + ' to ' + destination + ' is ' + distance);
+                } else {
+                    console.log(destination + ' is not reachable by land from ' + origin);
+                }
+            }
+        }
+    }
+});
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
